6cf1875980ca07a6f444fdf5a846f907159371b2
Rodolfo Araujo <rodoufu@gmail.com>
2022-10-19 14:21:09 -0300

    Binance checking errors.

--- src/binance.rs
+++ src/binance.rs
@@ -5,6 +5,8 @@ use futures_util::{
     pin_mut,
     StreamExt,
 };
+use opentelemetry::{Context, global, Key};
+use opentelemetry::trace::{FutureExt, TraceContextExt, Tracer};
 use serde_derive::{
     Deserialize,
     Serialize,
@@ -22,7 +24,7 @@ use tokio_tungstenite::{
 };
 use crate::types::{Level, Summary, WebsocketError};
 
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize)]
 struct DepthUpdate {
     bids: Vec<Vec<String>>,
     asks: Vec<Vec<String>>,
@@ -61,11 +63,15 @@ pub async fn run_binance(
     summary_tx: UnboundedSender<Summary>,
     symbol: &str, depth: u16,
 ) -> Result<(), Box<dyn std::error::Error>> {
+    let tracer = global::tracer("run_binance");
+    let span = tracer.start("running binance");
+    let cx = Context::current_with_span(span);
+
     let connect_addr = format!(
         "wss://stream.binance.com:9443/ws/{}@depth{}@100ms", symbol, depth,
     );
 
-    let url = url::Url::parse(&connect_addr).unwrap();
+    let url = url::Url::parse(&connect_addr)?;
 
     // let (stdin_tx, stdin_rx) = futures_channel::mpsc::unbounded();
     // tokio::spawn(read_stdin(stdin_tx));
@@ -76,28 +82,50 @@ pub async fn run_binance(
     let (write, read) = ws_stream.split();
 
     // let stdin_to_ws = stdin_rx.map(Ok).forward(write);
-    let ws_to_stdout = {
-        read.for_each(|message| async {
-            let mut binance_parse: serde_json::Result<DepthUpdate> = serde_json::from_slice(
-                &message.unwrap().into_data(),
-            );
+    read.for_each(|message| async {
+        let message_data = message.unwrap().into_data();
+        let mut binance_parse: serde_json::Result<DepthUpdate> = serde_json::from_slice(
+            &message_data,
+        );
 
-            match binance_parse {
-                Ok(mut depth_update) => {
-                    // depth_update.asks.resize(depth);
-                    // depth_update.bids.resize(depth);
-                    let summary = depth_update.try_into().ok().unwrap();
-                    summary_tx.send(summary).unwrap();
-                }
-                Err(err) => {
-
+        match binance_parse {
+            Ok(depth_update) => {
+                // depth_update.asks.resize(depth);
+                // depth_update.bids.resize(depth);
+                match depth_update.try_into() {
+                    Ok(summary) => {
+                        if let Err(err) = summary_tx.send(summary) {
+                            cx.span().add_event(
+                                "error information to the channel",
+                                vec![
+                                    Key::new("error").string(format!("{}", err)),
+                                ],
+                            );
+                        }
+                    }
+                    Err(err) => {
+                        cx.span().add_event(
+                            "error converting WebSocket data to domain type",
+                            vec![
+                                Key::new("error").string(format!("{:?}", err)),
+                            ],
+                        );
+                    }
                 }
             }
-        })
-    };
+            Err(err) => {
+                cx.span().add_event(
+                    "error parsing WebSocket data",
+                    vec![
+                        Key::new("message").string(format!("{:?}", message_data)),
+                        Key::new("error").string(format!("{}", err)),
+                    ],
+                );
+            }
+        }
+    }).with_context(cx.clone()).await;
 
-    // pin_mut!(stdin_to_ws, ws_to_stdout);
+    // pin_mut!(stdin_to_ws, ws_to_stdout)
     // future::select(stdin_to_ws, ws_to_stdout).await;
-    ws_to_stdout.await;
     Ok(())
 }
